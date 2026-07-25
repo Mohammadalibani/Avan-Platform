2ad0ac9b003144fb9231f99b2cb9b5cc5f00aefe
Mohammadalibani <malekbaniasadi@gmail.com>
2026-07-25 09:32:37 +0330

    Delete app directory

--- app/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-api_v1_bp = Blueprint('api_v1', __name__, url_prefix='/v1')
-
-# ✅ اضافه کردن fields و periods
-from app.api.v1 import auth, users, personnel, departments, units, requests, dashboard, fields, periods
--- app/models/departmentmanager.py
+++
@@ -1,8 +0,0 @@
-from app.extensions import db
-from flask_login import UserMixin
-
-class DepartmentManager(db.Model):
-    __tablename__ = 'department_managers'
-    id = db.Column(db.Integer, primary_key=True)
-    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
